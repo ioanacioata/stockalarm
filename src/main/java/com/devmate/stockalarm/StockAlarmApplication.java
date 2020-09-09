3e7768f64bc59f4a77269f00011dff3930d17d6c
ioana.cioata <Ioana.Cioata@sj.se>
2020-09-09 15:46:19 +0300

    Create initial project

---
+++ src/main/java/com/devmate/stockalarm/StockAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.devmate.stockalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAlarmApplication.class, args);
+	}
+
+}
